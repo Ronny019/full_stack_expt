64ad715f4c6434cd6dd0d760b0f8f7a28780b4e4
Ronny019 <mainak.pc@gmail.com>
2024-12-05 13:59:29 -0700

    changes to server.js

--- epiroc_assignment/server.js
+++ epiroc_assignment/server.js
@@ -88,6 +88,6 @@ app.delete("/api/expt/:id", async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
